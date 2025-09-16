ed67dba63c413010de1c73dda5d5385d785701dd
Deken <dekenitoha097@gmail.com>
2025-09-16 19:50:50 +0100

    version2

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,6 +5,6 @@ export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
     const avec = searchParams.get('avec');
     const meme = searchParams.get('meme');
-    const lui = searchParams.get("lui");
-    return NextResponse.json({ avec, meme ,lui})
+  
+    return NextResponse.json({ avec, meme })
 }
